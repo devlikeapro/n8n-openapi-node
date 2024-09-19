4b06c9f3a85b560cf534e9fc0c4132ac4d745f77
devlikepro <waha@devlike.pro>
2024-09-19 19:15:14 +0700

    dist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // export
 import {OpenAPIVisitor, OperationContext} from "./openapi/OpenAPIVisitor";
 import {OpenAPIWalker} from "./openapi/OpenAPIWalker";
-import {OpenAPIN8NParser, ParserConfig} from "./OpenAPIN8NParser";
+import {OpenAPIN8NParser, Override, ParserConfig} from "./OpenAPIN8NParser";
 import {OperationsCollector} from "./OperationsCollector";
 
 export {
@@ -11,4 +11,5 @@ export {
     OperationContext,
     OperationsCollector,
     ParserConfig,
+    Override,
 }
