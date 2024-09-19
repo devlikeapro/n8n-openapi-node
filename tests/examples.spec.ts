64725898b7f72de9977258fed4efcdb41a88b744
devlikepro <waha@devlike.pro>
2024-09-19 10:26:36 +0700

    add more tests

--- tests/examples.spec.ts
+++ tests/examples.spec.ts
@@ -1,7 +1,19 @@
 import {Parser} from "../src/parser";
-import * as doc from "./examples/petstore.json"
 
-test('petstore.json', () => {
+test('petstore.json', async () => {
+    const doc = require('./examples/petstore.json');
+    const parser = new Parser(doc);
+    parser.process()
+})
+
+test('waha.json', () => {
+    const doc = require('./examples/waha.json');
+    const parser = new Parser(doc);
+    parser.process()
+})
+
+test('chatwoot.json', () => {
+    const doc = require('./examples/chatwoot.json');
     const parser = new Parser(doc);
     parser.process()
 })
