5b813c8e9d826a2d873aa91966f097ccb8fbc409
devlikepro <waha@devlike.pro>
2024-09-20 16:14:35 +0700

    for each file in examples

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,20 +1,20 @@
 import {OpenAPIN8NParser} from "../src/OpenAPIN8NParser";
+import {readdirSync} from 'fs';
+import {join} from 'path';
+import {sleep} from "n8n-workflow";
 
-test('petstore.json', async () => {
-    const doc = require('./examples/petstore.json');
-    const parser = new OpenAPIN8NParser(doc);
-    parser.process()
-})
+// Define the path to your 'examples' directory
+const examplesDir = join(__dirname, 'examples');
 
+// Read all JSON files from the examples directory
+const jsonFiles = readdirSync(examplesDir).filter(file => file.endsWith('.json'));
 
-test('waha.json', async () => {
-    const doc = require('./examples/waha.json');
-    const parser = new OpenAPIN8NParser(doc);
-    parser.process()
-})
-
-test('chatwoot.json', () => {
-    const doc = require('./examples/chatwoot.json');
-    const parser = new OpenAPIN8NParser(doc);
-    parser.process()
-})
+describe('smoke', () => {
+    test.each(jsonFiles)('%s', async (fileName) => {
+        const filePath = join(examplesDir, fileName);
+        const doc = require(filePath);
+        const parser = new OpenAPIN8NParser(doc);
+        parser.process();
+        await sleep(50) // wait for logs a bit
+    });
+});
