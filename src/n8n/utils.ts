98a1d1c550229324cd7404edbdbc5f21950945e7
devlikepro <waha@devlike.pro>
2024-09-20 15:44:46 +0700

    start case

--- src/n8n/utils.ts
+++ src/n8n/utils.ts
@@ -1,4 +1,6 @@
+import * as lodash from "lodash";
+
 export function toResourceName(name: string) {
     // keep only ascii, no emojis
-    return name.replace(/[^a-zA-Z0-9]/g, '');
+    return lodash.startCase(name.replace(/[^a-zA-Z0-9]/g, ''))
 }
