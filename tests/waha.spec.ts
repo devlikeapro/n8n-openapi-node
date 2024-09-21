850680d7a0c482d9db9522def7e9a33664b76ba6
devlikepro <waha@devlike.pro>
2024-09-21 13:55:10 +0700

    Parsers

--- tests/waha.spec.ts
+++ tests/waha.spec.ts
@@ -2,6 +2,8 @@ import {N8NPropertiesBuilder, N8NPropertiesBuilderConfig} from "../src/N8NProper
 import {OperationsCollector} from "../src/OperationsCollector";
 import {OpenAPIV3} from "openapi-types";
 import {OperationContext} from "../src/openapi/OpenAPIVisitor";
+import {DefaultOperationParser} from "../src/OperationParser";
+import * as lodash from "lodash";
 
 function sessionFirst(a: any, b: any) {
     if (a.name === 'session') {
@@ -11,6 +13,28 @@ function sessionFirst(a: any, b: any) {
         return 1;
     }
     return 0;
+}
+
+export class WAHAOperationParser extends DefaultOperationParser {
+    name(operation: OpenAPIV3.OperationObject, context: OperationContext): string {
+        let operationId: string = operation.operationId!!.split('_').slice(1).join('_');
+        if (!operationId) {
+            operationId = operation.operationId as string
+        }
+        return lodash.startCase(operationId)
+    }
+
+    value(operation: OpenAPIV3.OperationObject, context: OperationContext): string {
+        return this.name(operation, context)
+    }
+
+    action(operation: OpenAPIV3.OperationObject, context: OperationContext): string {
+        return operation.summary || this.name(operation, context)
+    }
+
+    description(operation: OpenAPIV3.OperationObject, context: OperationContext): string {
+        return operation.description || operation.summary || '';
+    }
 }
 
 export class WAHAOperationsCollector extends OperationsCollector {
@@ -25,6 +49,7 @@ test('waha.json', () => {
     const doc = require('./examples/waha.json');
     const config: N8NPropertiesBuilderConfig = {
         OperationsCollector: WAHAOperationsCollector,
+        operation: new WAHAOperationParser(),
     }
     const parser = new N8NPropertiesBuilder(doc, config);
     const result = parser.build()
